7abc44d588f1041a4df5ca9ce0088d3180ad41e2
Varvara Utkina <varya2412@icloud.com>
2025-02-03 12:30:49 +0300

    Project 6: Day 32 of 100 days of SwiftUI with Paul Hudson

---
+++ Project 6/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Варвара Уткина on 03.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
