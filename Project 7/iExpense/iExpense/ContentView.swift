557811d239ba985a8dd6171b56c21ca8597f197e
Varvara Utkina <varya2412@icloud.com>
2025-02-06 13:08:20 +0300

    Project 7: Day 36 of 100 days of SwiftUI with Paul Hudson

---
+++ Project 7/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Варвара Уткина on 06.02.2025.
+//
+
+import SwiftUI
+
+struct User: Codable {
+    let firstName: String
+    let lastName: String
+}
+
+struct ContentView: View {
+    @State private var user = User(firstName: "taykor", lastName: "Swift")
+    
+    var body: some View {
+        Button("Save User") {
+            let encoder = JSONEncoder()
+            
+            if let data = try? encoder.encode(user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project 7/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Варвара Уткина on 06.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
