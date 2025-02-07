b9a238be33ab5d2eba5e3075f3045a764277f033
Varvara Utkina <varya2412@icloud.com>
2025-02-07 15:46:02 +0300

    Project 8: Day 40 of 100 days of SwiftUI with Paul Hudson

---
+++ Project 8/MoonShot/MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Варвара Уткина on 07.02.2025.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
