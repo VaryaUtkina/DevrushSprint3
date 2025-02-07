2fc306b56cf6965478814aefe1ba764e0d9bfdb7
Varvara Utkina <varya2412@icloud.com>
2025-02-07 14:21:53 +0300

    Make directory Project 8. Project 8: Day 39 of 100 days of SwiftUI with Paul Hudson

---
+++ Project 8/MoonShot/MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Варвара Уткина on 07.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
